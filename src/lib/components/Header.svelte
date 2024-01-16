6e25e485e14f2cfe6b8982d9dd4ac63aa4e98fb6
SaurabPoudel <poudelsaurab20@gmail.com>
2024-01-16 22:19:06 +0545

    start

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+	import SearchBar from './SearchBar.svelte';
+</script>
+
+<header
+	class="flex px-4 items-center h-[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0"
+>
+	<a href="/">
+		<img src="/icons/ytLogo.svg" class="md:hidden mr-5" alt="yt logo" />
+	</a>
+	<div class="md:flex hidden gap-4 items-center">
+		<img
+			src="/icons/bugerIcon.svg"
+			class="cursor-pointer p-3 rounded-full hover:bg-gray-200"
+			alt="hamburger icon"
+		/>
+		<a href="/">
+			<img src="/icons/ytLogo.svg" alt="yt logo" />
+		</a>
+	</div>
+	<SearchBar />
+	<div class="md:flex hidden gap-3 items-center">
+		<img
+			src="/icons/createIcon.svg"
+			class="cursor-pointer p-3 rounded-full hover:bg-gray-200"
+			alt="create icons"
+		/>
+		<img
+			src="/icons/notIcon.svg"
+			class="cursor-pointer p-3 rounded-full hover:bg-gray-200"
+			alt="not icons"
+		/>
+		<img src="/avatar.png" class="bg-gray-200 h-8 w-8 rounded-full cursor-pointer" alt="avatar" />
+	</div>
+</header>
---
+++ src/lib/components/SideBar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	const nav = [
+		{
+			icon: 'homeIcon',
+			title: 'home'
+		},
+		{
+			icon: 'shortsIcon',
+			title: 'shorts'
+		},
+		{
+			icon: 'libraryIcon',
+			title: 'library'
+		},
+		{
+			icon: 'subIcon',
+			title: 'subscription'
+		}
+	];
+</script>
+
+<div class="bg-white py-2 flex flex-col px-1 fixed top-[60px]">
+	{#each nav as item}
+		<a
+			href={item.title}
+			class="flex items-center justify-center flex-col gap-1 py-4
+  hover:bg-gray-100 rounded-lg"
+		>
+			<img src={`icons/${item.icon}.svg`} alt={item.icon} />
+			<p class="">{item.title}</p>
+		</a>
+	{/each}
+</div>
